4a8c0127a930c3a5b42d6a409bfa0cb6e65f078f
ZhengBoWei1002 <1653547092@qq.com>
2026-03-23 14:18:00 +0800

    Implement Reviewer Agent to determine: -Is the information sufficient -Do you need to search again

---
+++ AI_Research_Agent/src/ai_research_agent/agents/reviewer/__init__.py
@@ -0,0 +1,6 @@
+"""Reviewer agent package."""
+
+from ai_research_agent.agents.reviewer.agent import ReviewerAgent
+from ai_research_agent.agents.reviewer.state import ReviewerOutput
+
+__all__ = ["ReviewerAgent", "ReviewerOutput"]
